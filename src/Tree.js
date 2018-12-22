ac6618861c07db84fef93f766f8c636aaa3d3dac
Yuval Levy <ylevy12@gmail.com>
2018-12-22 03:35:48 +0200

    Placing elements over a specific word

--- src/Tree.js
+++ src/Tree.js
@@ -1,9 +1,69 @@
 import React, { Component } from 'react';
+import { measureText } from './measureText';
+
+const SENTENCE = 'Colorless green ideas sleep furiously.';
+const TREE = {
+  cat: 'S',
+  children: [
+    {
+      cat: 'NP',
+      children: [
+        {
+          cat: 'Adj',
+          slice: [0, 9]
+        },
+        {
+          cat: 'NP',
+          children: [
+            {
+              cat: 'Adj',
+              slice: [10, 15]
+            },
+            {
+              cat: 'N',
+              slice: [16, 21]
+            }
+          ]
+        }
+      ]
+    },
+    {
+      cat: 'VP',
+      children: [
+        {
+          cat: 'V',
+          slice: [22, 27]
+        },
+        {
+          cat: 'AdvP',
+          children: [
+            {
+              cat: 'Adv',
+              slice: [28, 37]
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
 
 class Tree extends Component {
+  sliceWidth = (sentence, start, end) => measureText(sentence.slice(start, end));
+
+  sliceX = (sentence, start) => this.sliceWidth(sentence, 0, start);
+
+  renderSvg = (sentence, tree) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width={500} height={500}>
+      <rect x={this.sliceX(sentence, 22)} y={380} width={this.sliceWidth(sentence, 22, 27)} height={40} fill="lightgreen" />
+      <text x={0} y={400}>{sentence}</text>
+    </svg>
+  )
+
   render() {
     return (
       <div className="tree">
+        {this.renderSvg(SENTENCE, TREE)}
       </div>
     );
   }
