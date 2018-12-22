e900d49cf9bf1488e8a6f0776b3061b4b9a8e930
Yuval Levy <ylevy12@gmail.com>
2018-12-22 02:34:38 +0200

    Makeshift text width measurer

---
+++ src/measureText.js
@@ -0,0 +1,18 @@
+const MEASUREMENT_ELEMENT_ID = 'text-measurement';
+
+export function measureText(str) {
+  let el = document.getElementById(MEASUREMENT_ELEMENT_ID);
+  if (!el) {
+    const measurementElement = document.createElement('span');
+    measurementElement.id = MEASUREMENT_ELEMENT_ID;
+    measurementElement.style.position = 'absolute';
+    measurementElement.style.left = -99999;
+    measurementElement.style.top = -99999;
+    measurementElement.style.padding = 0;
+    measurementElement.style.whiteSpace = 'pre';
+    document.body.appendChild(measurementElement);
+    el = document.getElementById(MEASUREMENT_ELEMENT_ID);
+  }
+  el.innerText = str;
+  return el.getBoundingClientRect().width;
+}
