665fd3d9c3c1f4b1910737fe57208c683b8433bf
Yuval Levy <ylevy12@gmail.com>
2018-12-22 03:24:24 +0200

    Tree component template

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Tree from './Tree.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <Tree />
       </div>
     );
   }
---
+++ src/Tree.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Tree extends Component {
+  render() {
+    return (
+      <div className="tree">
+      </div>
+    );
+  }
+}
+
+export default Tree;
