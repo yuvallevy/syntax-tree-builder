780937d11f99c1667ead3b08358ccce6f17064b8
Yuval Levy <ylevy12@gmail.com>
2019-01-27 23:47:34 +0200

    New node functionality

--- src/Editor.js
+++ src/Editor.js
@@ -8,29 +8,47 @@ class Editor extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      nodes: [],
       sentence: SENTENCE,
-      selectedRange: null
+      selectedRange: null,
+      selectedNodes: null
     };
   }
 
-  onSentenceChanged = (newSentence) => {
+  onSentenceChanged = newSentence => {
     this.setState({sentence: newSentence});
   }
 
   onSelectionChanged = (start, end) => {
-    this.setState({selectedRange: [start, end]});
+    this.setState({
+      selectedRange: [start, end],
+      selectedNodes: null
+    });
+  }
+
+  onNodeAdded = () => {
+    if (this.state.selectedRange) {
+      this.setState({
+        nodes: [...this.state.nodes, {
+          label: 'P',
+          slice: this.state.selectedRange
+        }]
+      });
+    }
   }
 
   render() {
     return (
       <div className="Editor">
         <View
+          nodes={this.state.nodes}
           sentence={this.state.sentence}
           onSentenceChanged={this.onSentenceChanged}
           onSelectionChanged={this.onSelectionChanged}
         />
         <Controls
-          selectedRange={this.state.selectedRange}
+          sentence={this.state.sentence}
+          onNodeAdded={this.onNodeAdded}
         />
       </div>
     )
