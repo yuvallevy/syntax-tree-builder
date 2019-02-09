4469c03c0fa53545cef417972838dbb9b64e347c
Yuval Levy <ylevy12@gmail.com>
2019-02-09 22:28:03 +0200

    Prepare for automatic positioning

--- src/ViewSvg.js
+++ src/ViewSvg.js
@@ -2,8 +2,24 @@ import React, { Component } from 'react';
 import { measureText } from './measureText';
 
 class ViewSvg extends Component {
+  determineNodeX = (node) => {
+    return 0;
+  }
+
+  determineNodeY = (node) => {
+    return 190;
+  }
+
   renderNodes = () => this.props.nodes.map(
-    node => <text x="100" y="100">{node.label}</text>
+    node => (
+      <text
+        x={this.determineNodeX(node)}
+        y={this.determineNodeY(node)}
+        textAnchor="middle"
+      >
+        {node.label}
+      </text>
+    )
   );
 
   render() {
