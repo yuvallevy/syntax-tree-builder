fb8c09e6bd6349378a227724a40157a6227339a2
Yuval Levy <ylevy12@gmail.com>
2019-03-06 01:24:54 +0200

    Doc positioning functions

--- src/ViewSvg.js
+++ src/ViewSvg.js
@@ -7,16 +7,42 @@ class ViewSvg extends Component {
     positionedNodes: null
   };
 
+  /**
+   * Calculates the X position of a leaf node corresponding to the given sentence slice.
+   * @param  {number} start Start position of the slice (inclusive).
+   * @param  {number} end   End position of the slice (exclusive).
+   * @return {number}       Node's target X position.
+   */
   computeXBySlice = (start, end) => measureText(this.props.sentence.slice(0, start)) +
     (measureText(this.props.sentence.slice(start, end)) / 2);
 
+  /**
+   * Calculates the X position of a node with the given children.
+   * @param  {Array}  children List of children.
+   * @return {number}          Node's target X position.
+   */
   computeXByChildren = (children) => avg(children.map(childId => this.computeNodeX(this.props.nodes[childId])));
 
+  /**
+   * Calculates the X position of the given node.
+   * @param  {*}      node Node to position.
+   * @return {number}      Node's target X position.
+   */
   computeNodeX = (node) => node.slice ? this.computeXBySlice(...node.slice) : this.computeXByChildren(node.children);
 
+  /**
+   * Calculates the Y position of a node with the given children.
+   * @param  {Array}  children List of children.
+   * @return {number}          Node's target Y position.
+   */
   computeYByChildren = (children) =>
     Math.min(...children.map(childId => this.computeNodeY(this.props.nodes[childId]))) - 40;
 
+  /**
+   * Calculates the Y position of the given node.
+   * @param  {*}      node Node to position.
+   * @return {number}      Node's target Y position.
+   */
   computeNodeY = (node) => node.slice ? 190 : this.computeYByChildren(node.children);
 
   computeNodePositions = () => Object.entries(this.props.nodes).map(([id, node]) => ({
