283ed4542812092699538ab99768cb38bcedd348
Yuval Levy <ylevy12@gmail.com>
2019-03-18 00:45:28 +0200

    Empty initial tree

--- src/Editor.js
+++ src/Editor.js
@@ -9,44 +9,7 @@ class Editor extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      nodes: {
-        0: {
-          label: 'Adj',
-          slice: [0, 9]
-        },
-        1: {
-          label: 'Adj',
-          slice: [10, 15]
-        },
-        2: {
-          label: 'N',
-          slice: [16, 21]
-        },
-        3: {
-          label: 'V',
-          slice: [22, 27]
-        },
-        4: {
-          label: 'Adv',
-          slice: [28, 37]
-        },
-        5: {
-          label: 'NP',
-          children: [1, 2]
-        },
-        6: {
-          label: 'NP',
-          children: [0, 5]
-        },
-        7: {
-          label: 'VP',
-          children: [3, 4]
-        },
-        8: {
-          label: 'S',
-          children: [6, 7]
-        }
-      },
+      nodes: {},
       sentence: SENTENCE,
       selectedRange: null,
       selectedNodes: null
--- src/examples.js
+++ src/examples.js
@@ -1,1 +1,39 @@
 export const SENTENCE = 'Colorless green ideas sleep furiously.';
+export const TREE = {
+  0: {
+    label: 'Adj',
+    slice: [0, 9]
+  },
+  1: {
+    label: 'Adj',
+    slice: [10, 15]
+  },
+  2: {
+    label: 'N',
+    slice: [16, 21]
+  },
+  3: {
+    label: 'V',
+    slice: [22, 27]
+  },
+  4: {
+    label: 'Adv',
+    slice: [28, 37]
+  },
+  5: {
+    label: 'NP',
+    children: [1, 2]
+  },
+  6: {
+    label: 'NP',
+    children: [0, 5]
+  },
+  7: {
+    label: 'VP',
+    children: [3, 4]
+  },
+  8: {
+    label: 'S',
+    children: [6, 7]
+  }
+};
