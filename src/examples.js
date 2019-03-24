002eaeb5652e33735b8ca980f7c690cd0a25030d
Yuval Levy <ylevy12@gmail.com>
2019-03-24 23:54:12 +0200

    Update mock data to match new structure

--- src/examples.js
+++ src/examples.js
@@ -1,38 +1,47 @@
 export const SENTENCE = 'Colorless green ideas sleep furiously.';
 export const TREE = {
   0: {
+    id: 0,
     label: 'Adj',
     slice: [0, 9]
   },
   1: {
+    id: 1,
     label: 'Adj',
     slice: [10, 15]
   },
   2: {
+    id: 2,
     label: 'N',
     slice: [16, 21]
   },
   3: {
+    id: 3,
     label: 'V',
     slice: [22, 27]
   },
   4: {
+    id: 4,
     label: 'Adv',
     slice: [28, 37]
   },
   5: {
+    id: 5,
     label: 'NP',
     children: [1, 2]
   },
   6: {
+    id: 6,
     label: 'NP',
     children: [0, 5]
   },
   7: {
+    id: 7,
     label: 'VP',
     children: [3, 4]
   },
   8: {
+    id: 8,
     label: 'S',
     children: [6, 7]
   }
