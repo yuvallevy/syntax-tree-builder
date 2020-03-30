add05a12a79fbf22edfe055404c0c82b8f9d0b60
Yuval Levy III <ylevy12@gmail.com>
2020-03-30 09:56:12 +0300

    Hookify ViewSvg

--- src/ViewSvg.tsx
+++ src/ViewSvg.tsx
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { measureText } from './measureText';
 import { avg } from './utils';
 import { flatMap } from 'lodash';
-import deepEqual from 'deep-equal';
 import { NodeTree, NodeData, NodeId } from './interfaces';
 import './ViewSvg.scss';
 
@@ -24,97 +23,111 @@ interface PositionedNodeTree {
   [nodeId: string]: PositionedNodeData;
 }
 
-interface PositionCache {
-  [nodeId: string]: number;
-}
+type PositionCache = Map<NodeId, number>;
 
 interface ViewSvgState {
   positionedNodes: PositionedNodeTree
 }
 
-class ViewSvg extends Component<ViewSvgProps, ViewSvgState> {
-  state: ViewSvgState = {
-    positionedNodes: {}
-  };
+const xCache: PositionCache = new Map();
+const yCache: PositionCache = new Map();
 
-  xCache: PositionCache = {};
-  yCache: PositionCache = {};
+/**
+ * Calculates the X position of a leaf node corresponding to the given sentence slice.
+ * @param  {string} sentence Sentence to measure against.
+ * @param  {number} start    Start position of the slice (inclusive).
+ * @param  {number} end      End position of the slice (exclusive).
+ * @return {number}          Node's target X position.
+ */
+const computeXBySlice = (sentence: string, start: number, end: number): number =>
+measureText(sentence.slice(0, start)) + (measureText(sentence.slice(start, end)) / 2);
 
-  /**
-   * Calculates the X position of a leaf node corresponding to the given sentence slice.
-   * @param  {number} start Start position of the slice (inclusive).
-   * @param  {number} end   End position of the slice (exclusive).
-   * @return {number}       Node's target X position.
-   */
-  computeXBySlice = (start: number, end: number): number => measureText(this.props.sentence.slice(0, start)) +
-    (measureText(this.props.sentence.slice(start, end)) / 2);
+/**
+ * Calculates the X position of a node with the given children.
+ * @param  {NodeTree} nodes    Tree of nodes.
+ * @param  {string}   sentence Sentence to measure against.
+ * @param  {NodeId[]} children List of children.
+ * @return {number}            Node's target X position.
+ */
+const computeXByChildren = (nodes: NodeTree, sentence: string, children: NodeId[]) =>
+  avg(children.map(childId => getNodeX(nodes, sentence, nodes[childId])));
 
-  /**
-   * Calculates the X position of a node with the given children.
-   * @param  {NodeId[]} children List of children.
-   * @return {number}            Node's target X position.
-   */
-  computeXByChildren = (children: NodeId[]) => avg(children.map(childId => this.getNodeX(this.props.nodes[childId])));
-
-  /**
-   * Calculates the X position of the given node.
-   * @param  {NodeData} node Node to position.
-   * @return {number}        Node's target X position.
-   */
-  computeNodeX = (node: NodeData) => node.slice ? this.computeXBySlice(...node.slice)
-    : node.children ? this.computeXByChildren(node.children)
+/**
+ * Calculates the X position of the given node.
+ * @param  {NodeTree} nodes    Tree of nodes.
+ * @param  {string}   sentence Sentence to measure against.
+ * @param  {NodeData} node     Node to position.
+ * @return {number}            Node's target X position.
+ */
+const computeNodeX = (nodes: NodeTree, sentence: string, node: NodeData) =>
+  node.slice ? computeXBySlice(sentence, ...node.slice)
+    : node.children ? computeXByChildren(nodes, sentence, node.children)
       : 0;
 
-  /**
-   * Calculates the Y position of a node with the given children.
-   * @param  {NodeId[]} children List of children.
-   * @return {number}            Node's target Y position.
-   */
-  computeYByChildren = (children: NodeId[]) =>
-    Math.min(...children.map(childId => this.getNodeY(this.props.nodes[childId]))) - 40;
+/**
+ * Calculates the Y position of a node with the given children.
+ * @param  {NodeTree} nodes    Tree of nodes.
+ * @param  {NodeId[]} children List of children.
+ * @return {number}            Node's target Y position.
+ */
+const computeYByChildren = (nodes: NodeTree, children: NodeId[]) =>
+  Math.min(...children.map(childId => getNodeY(nodes, nodes[childId]))) - 40;
 
-  /**
-   * Calculates the Y position of the given node.
-   * @param  {NodeData} node Node to position.
-   * @return {number}        Node's target Y position.
-   */
-  computeNodeY = (node: NodeData) => node.slice ? 178
-    : node.children ? this.computeYByChildren(node.children)
-      : 0;
+/**
+ * Calculates the Y position of the given node.
+ * @param  {NodeTree} nodes Tree of nodes.
+ * @param  {NodeData} node  Node to position.
+ * @return {number}         Node's target Y position.
+ */
+const computeNodeY = (nodes: NodeTree, node: NodeData) => node.slice ? 178
+  : node.children ? computeYByChildren(nodes, node.children)
+    : 0;
 
-  /**
-   * Returns the X position of the given node and caches the result, or retrieves it if it is already cached.
-   * @param  {NodeData} node Node to position.
-   * @return {number}        Node's target X position.
-   */
-  getNodeX = (node: NodeData) => {
-    if (!this.xCache[node.id]) {
-      this.xCache[node.id] = this.computeNodeX(node);
-    }
-    return this.xCache[node.id];
-  };
+/**
+ * Returns the X position of the given node and caches the result, or retrieves it if it is already cached.
+ * @param  {NodeTree} nodes    Tree of nodes.
+ * @param  {string}   sentence Sentence to measure against.
+ * @param  {NodeData} node     Node to position.
+ * @return {number}            Node's target X position.
+ */
+const getNodeX = (nodes: NodeTree, sentence: string, node: NodeData): number => {
+  if (!xCache.has(node.id)) {
+    xCache.set(node.id, computeNodeX(nodes, sentence, node));
+  }
+  return xCache.get(node.id) || 0;
+};
 
-  /**
-   * Returns the Y position of the given node and caches the result, or retrieves it if it is already cached.
-   * @param  {NodeData} node Node to position.
-   * @return {number}        Node's target Y position.
-   */
-  getNodeY = (node: NodeData) => {
-    if (!this.yCache[node.id]) {
-      this.yCache[node.id] = this.computeNodeY(node);
-    }
-    return this.yCache[node.id];
-  };
+/**
+ * Returns the Y position of the given node and caches the result, or retrieves it if it is already cached.
+ * @param  {NodeTree} nodes Tree of nodes.
+ * @param  {NodeData} node  Node to position.
+ * @return {number}         Node's target Y position.
+ */
+const getNodeY = (nodes: NodeTree, node: NodeData): number => {
+  if (!yCache.has(node.id)) {
+    yCache.set(node.id, computeNodeY(nodes, node));
+  }
+  return yCache.get(node.id) || 0;
+};
 
-  computeNodePositions = (): PositionedNodeTree => {
-    this.xCache = {};
-    this.yCache = {};
-    return Object.entries(this.props.nodes).reduce((positionedNodes, [id, node]) => ({
+const ViewSvg: React.FC<ViewSvgProps> = ({ nodes, sentence, selectedNodes, editingNode, onNodeSelected, onNodeLabelChanged }) => {
+  const [positionedNodes, setPositionedNodes] = useState<PositionedNodeTree>({});
+  console.log(positionedNodes);
+
+  useEffect(() => {
+    console.log(nodes);
+    setPositionedNodes(computeNodePositions());
+  }, [nodes]);
+
+  const computeNodePositions = (): PositionedNodeTree => {
+    xCache.clear();
+    yCache.clear();
+    return Object.entries(nodes).reduce((positionedNodes, [id, node]) => ({
       ...positionedNodes,
       [id]: {
         ...node,
-        x: this.getNodeX(node),
-        y: this.getNodeY(node)
+        x: getNodeX(nodes, sentence, node),
+        y: getNodeY(nodes, node)
       }
     }), {});
   }
@@ -123,42 +136,42 @@ class ViewSvg extends Component<ViewSvgProps, ViewSvgState> {
    * Sets a node as selected.
    * @param event Event that triggered the selection.
    */
-  selectNode = (event: React.MouseEvent<SVGElement> | React.TouchEvent<SVGElement>) => {
+  const selectNode = (event: React.MouseEvent<SVGElement> | React.TouchEvent<SVGElement>) => {
     const { nodeId } = event.currentTarget.dataset;
     if (nodeId) {
-      this.props.onNodeSelected(nodeId, event.ctrlKey);
+      onNodeSelected(nodeId, event.ctrlKey);
     }
   };
-  
+
   /**
    * Sets a node's label.
    * @param event Event that triggered the update.
    */
-  setNodeLabel = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const setNodeLabel = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { nodeId } = event.currentTarget.dataset;
     if (nodeId) {
-      this.props.onNodeLabelChanged(nodeId, event.target.value);
+      onNodeLabelChanged(nodeId, event.currentTarget.value);
     }
   }
 
-  renderNodes = () => Object.entries(this.state.positionedNodes).map(
+  const renderNodes = () => Object.entries(positionedNodes).map(
     ([nodeId, node]) => (
       <g
         key={nodeId}
-        className={this.props.selectedNodes && this.props.selectedNodes.has(nodeId) ? 'node selected' : 'node'}
+        className={selectedNodes && selectedNodes.has(nodeId) ? 'node selected' : 'node'}
       >
         <rect
           x={Math.round(node.x - 14)} y={Math.round(node.y)}
           data-node-id={nodeId}
           width={28} height={22}
-          onMouseDown={this.selectNode}
-          onTouchStart={this.selectNode}
+          onMouseDown={selectNode}
+          onTouchStart={selectNode}
         />
         <text
           x={node.x} y={node.y}
           data-node-id={nodeId}
-          onMouseDown={this.selectNode}
-          onTouchStart={this.selectNode}
+          onMouseDown={selectNode}
+          onTouchStart={selectNode}
         >
           {node.label}
         </text>
@@ -166,27 +179,9 @@ class ViewSvg extends Component<ViewSvgProps, ViewSvgState> {
     )
   );
 
-  renderEditingNode = (): React.ReactNode => {
-    const node: PositionedNodeData | null =
-      this.props.editingNode ? this.state.positionedNodes[this.props.editingNode] : null;
-    return node && <input
-      type="text"
-      className="node-edit-box"
-      data-node-id={node.id}
-      value={node.label}
-      style={{
-        left: node.x - 16,
-        top: node.y,
-        width: 32
-      }}
-      onChange={this.setNodeLabel}
-      autoFocus={true}
-    />;
-  }
-
-  renderLinks = () => flatMap(Object.values(this.state.positionedNodes),
+  const renderLinks = () => flatMap(Object.values(positionedNodes),
     (node: PositionedNodeData) => node.children ? node.children.map(childId => {
-      const child: PositionedNodeData = this.state.positionedNodes[childId];
+      const child: PositionedNodeData = positionedNodes[childId];
       return <line
         key={childId}
         className="tree-link"
@@ -198,31 +193,30 @@ class ViewSvg extends Component<ViewSvgProps, ViewSvgState> {
     }) : []
   );
 
-  updateNodePositions = (): void => {
-    this.setState({positionedNodes: this.computeNodePositions()});
+  const renderEditingNode = (): React.ReactNode => {
+    const node: PositionedNodeData | null = editingNode ? positionedNodes[editingNode] : null;
+    return node && <input
+      type="text"
+      className="node-edit-box"
+      data-node-id={node.id}
+      value={node.label}
+      style={{
+        left: node.x - 16,
+        top: node.y,
+        width: 32
+      }}
+      onChange={setNodeLabel}
+      autoFocus={true}
+    />;
   }
 
-  componentDidMount() {
-    this.updateNodePositions();
-  }
-
-  componentDidUpdate(prevProps: ViewSvgProps) {
-    if (!deepEqual(prevProps.nodes, this.props.nodes)) {
-      this.updateNodePositions();
-    }
-  }
-
-  render() {
-    return (
-      <div className="ViewSvg">
-        <svg width={measureText(this.props.sentence)} height={200}>
-          {this.renderNodes()}
-          {this.renderLinks()}
-        </svg>
-        {this.props.editingNode && this.renderEditingNode()}
-      </div>
-    )
-  }
-}
+  return <div className="ViewSvg">
+    <svg width={measureText(sentence)} height={200}>
+      {renderNodes()}
+      {renderLinks()}
+    </svg>
+    {editingNode && renderEditingNode()}
+  </div>;
+};
 
 export default ViewSvg;
