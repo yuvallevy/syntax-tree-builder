4169908fc240ec35b83d8bc6cc4c0d56777ae551
Yuval Levy III <ylevy12@gmail.com>
2020-05-22 19:08:09 +0300

    Shift nodes when changing text

--- src/reducers/EditorReducer.ts
+++ src/reducers/EditorReducer.ts
@@ -182,7 +182,40 @@ const completeDisowning = (state: EditorState, nodeIds: NodeId[] | null): Editor
     disowningNode: null,
     editingNode: null
   };
-}
+};
+
+/**
+ * Shifts the slice of a node, if present, to accommodate new characters inserted at a particular position.
+ */
+const shiftNodeSlice = (node: NodeData, insertedCount: number, insertedAt: number): NodeData => {
+  if (node.slice) {
+    const newNodeSlice: [number, number] = [
+      node.slice[0] > insertedAt ? node.slice[0] + insertedCount : node.slice[0],
+      node.slice[1] >= insertedAt ? node.slice[1] + insertedCount : node.slice[1]
+    ];
+    return {
+      ...node,
+      slice: newNodeSlice[1] > newNodeSlice[0] ? newNodeSlice : undefined
+    };
+  }
+  return node;
+};
+
+const setSentence = (state: EditorState, newSentence: string): EditorState => {
+  const lengthDiff = newSentence.length - state.sentence.length;
+  const cursorPosition = state.selectedRange?.[0];
+  const newNodes: NodeTree = cursorPosition !== undefined
+    ? Object.entries(state.nodes).reduce((acc, [nodeId, node]) => ({
+      ...acc,
+      [nodeId]: shiftNodeSlice(node, lengthDiff, cursorPosition)
+    }), {})
+    : state.nodes;
+  return {
+    ...state,
+    nodes: newNodes,
+    sentence: newSentence
+  };
+};
 
 const selectText = (state: EditorState, start: number, end: number): EditorState =>
   state.adoptingNode
@@ -351,7 +384,7 @@ const resetNodePositions = (state: EditorState): EditorState => {
 export const reducer = (state: EditorState, action: EditorAction): EditorState => {
   console.log(action);
   switch (action.type) {
-    case 'setSentence': return { ...state, sentence: action.newSentence };
+    case 'setSentence': return setSentence(state, action.newSentence);
     case 'selectText': return selectText(state, action.start, action.end);
     case 'selectNode': return selectNode(state, action.nodeIds, action.multi);
     case 'clearSelection': return { ...state, selectedNodes: null, editingNode: null };
