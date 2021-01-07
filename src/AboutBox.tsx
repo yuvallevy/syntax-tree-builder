7bc227c49990800160a941a03cccd68c0c745ace
Yuval Levy <ylevy12@gmail.com>
2021-01-07 23:43:55 +0200

    About box edits

--- src/AboutBox.tsx
+++ src/AboutBox.tsx
@@ -20,7 +20,7 @@ const AboutBox: React.FC<{}> = () => {
     onDismiss={hideMe}
   >
     <p>Build <strong>constituent trees</strong> for articles, lectures, homework etc.
-      using an intuitive, in-browser WYSIWYG interface, without labeled bracket notation.</p>
+      using an intuitive, in-browser WYSIWYG interface instead of labeled bracket notation.</p>
     <h4>New in alpha {CURRENT_VERSION}:</h4>
     <ul>
       <li>Fixed bug where top-level nodes were able to adopt themselves,
@@ -33,12 +33,12 @@ const AboutBox: React.FC<{}> = () => {
       </a> for full release notes.
     </p>
     <p>
-      Proudly powered by <a href="https://reactjs.org/" target="_blank">
-        React
-      </a>, <a href="https://www.w3.org/Graphics/SVG/" target="_blank">
-        SVG
-      </a> and <a href="https://www.typescriptlang.org/" target="_blank">
+      Syntax Tree Builder is written in <a href="https://www.typescriptlang.org/" target="_blank">
         TypeScript
+      </a> and powered by <a href="https://reactjs.org/" target="_blank">
+        React
+      </a> and <a href="https://www.w3.org/Graphics/SVG/" target="_blank">
+        SVG
       </a>.
     </p>
   </Modal>
