0851008c7272e6696adc388462736b606f210329
Yuval Levy <ylevy12@gmail.com>
2021-02-21 18:28:00 +0200

    Separate definition from export

--- src/generateId.ts
+++ src/generateId.ts
@@ -2,4 +2,6 @@
  * Generates a random string ID 12 characters long,
  * consisting of a combination of uppercase and lowercase letters, @, [, \, ], ^, _, `, {, |, and }.
  */
-export default (): string => new Array(12).fill(null).map(i => String.fromCharCode(Math.floor(Math.random() * 62 + 64))).join('');
+const generateId = (): string => new Array(12).fill(null).map(i => String.fromCharCode(Math.floor(Math.random() * 62 + 64))).join('');
+
+export default generateId;
